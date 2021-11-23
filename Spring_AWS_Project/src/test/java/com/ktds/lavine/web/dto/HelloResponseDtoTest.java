5be2427a476f474246ecf5bdd842257a0333b05f
jihun <zihun35@gmail.com>
2021-11-23 17:46:11 +0900

    rombok test 수행

---
+++ Spring_AWS_Project/src/test/java/com/ktds/lavine/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.ktds.lavine.web.dto;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+	
+	@Test
+	public void rombok_test() {
+		String name = "test";
+		int amount = 1000;
+		
+		HelloResponseDto dto = new HelloResponseDto(name, amount);
+		
+		assertThat(name, equalTo(dto.getName()));
+		assertThat(amount, equalTo(dto.getAmount()));
+	}
+}
