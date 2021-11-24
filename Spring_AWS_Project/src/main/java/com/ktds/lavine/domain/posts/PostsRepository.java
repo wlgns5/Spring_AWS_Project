f575b74e29471064fd8f8b873a0052f6e98891e7
jihun <zihun35@gmail.com>
2021-11-24 13:23:04 +0900

    JPA Code 추가

--- Spring_AWS_Project/src/main/java/com/ktds/lavine/domain/posts/PostsRepository.java
+++ Spring_AWS_Project/src/main/java/com/ktds/lavine/domain/posts/PostsRepository.java
@@ -1,5 +1,7 @@
 package com.ktds.lavine.domain.posts;
 
-public interface PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+	
 }
