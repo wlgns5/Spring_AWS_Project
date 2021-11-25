53ab163d24e9bdea66116c0020dfa6767e4c9e09
jihun <zihun35@gmail.com>
2021-11-25 14:15:41 +0900

    junit 4 -> junit 5 변경

--- Spring_AWS_Project/src/test/java/com/ktds/lavine/web/HelloControllerTest.java
+++ Spring_AWS_Project/src/test/java/com/ktds/lavine/web/HelloControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.DynamicTest.stream;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
